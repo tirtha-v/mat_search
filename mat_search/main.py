5e0126eb4ff40b16cfd9afdaa3ed239b348e5fe9
Tirtha Vinchurkar <tsvin642@gmail.com>
2024-04-25 11:01:30 -0400

    Update main.py
    made changes in click.argument

--- mat_search/main.py
+++ mat_search/main.py
@@ -6,10 +6,10 @@ from mp_api.client import MPRester
 @click.command(
     help="Material search with API key, property, range, and number of elements"
 )
-@click.argument("api_key", typ=str)
-@click.argument("property", typ=str)
-@click.argument("min", typ=float)
-@click.argument("max", typ=float)
+@click.argument("api_key", type=str)
+@click.argument("property", type=str)
+@click.argument("min", type=float)
+@click.argument("max", type=float)
 @click.argument("num", type=int)
 def main(api_key, property, min, max, num):
     print("\n".join(get_mat(api_key, property, float(min), float(max), int(num))))
